e30428dfe64536439145572b17a017753381fb6f
beomsu <55176563+ta09472@users.noreply.github.com>
2021-07-23 15:50:07 +0900

    Update client.py

--- simple_TCP_Chat/client.py
+++ simple_TCP_Chat/client.py
@@ -1,29 +1,33 @@
 import socket
 import threading
 
+# 클라이언트는 닉네임을 입력하고 서버에 연결한다.
 nickname = input("Choose a nickname: ")
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 55554))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # 서버와 동일한 소켓과 프로토콜 선언
+client.connect(('127.0.0.1', 55554)) # 서버와 동일한 IP와 PORT에 접속
 
-def receive():
+# 클라이언트의 첫번쩨 스레드
+def receive(): # 서버로부터 데이터를 받는다.
     while True:
-        try:
+        try: # 메세지를 수신하고 출력한다.
             message = client.recv(1024).decode('ascii')
             if message == "NICK":
                 client.send(nickname.encode('ascii'))
             else:
                 print(message)
-        except:
+        except: # 예외 발생시 연결을 차단한다. 
             print("An error occurred!")
             client.close()
             break
-
-def write():
+            
+# 클라이언트의 두번째 스레드
+def write(): # 서버에 메세지를 보낸다.
     while True:
         message = f'{nickname}: {input("")}'
         client.send(message.encode('ascii'))
 
+# 송수신을 병렬적으로 처리
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
 
